c2dd8c03664ca4dfbb7edf16e4cca9d2ebb18e14
liuzhijian <liu.zhijian@intellif.com>
2018-12-21 18:43:38 +0800

    新增 netty示例

--- src/test/java/test/NormalTest.java
+++ src/test/java/test/NormalTest.java
@@ -1,27 +1,8 @@
 package test;
 
-import com.liuzj.mybatis.common.utils.ArrayUtil;
-import com.liuzj.mybatis.common.utils.ParamNameUtil;
-import com.sun.deploy.util.ParameterUtil;
-
-import java.util.List;
-
+/**
+ * CountDownLatch 等待所有线程都执行完成之后再接着主线程执行
+ */
 public class NormalTest {
-    public static void main(String[] args) throws NoSuchMethodException {
-        NormalTest normalTest = new NormalTest();
-        List<String> params = ParamNameUtil.getParamNames(normalTest.getClass().getMethod("testMethod", String.class));
-    }
-
-    public void testMethod(String aa){
-        {
-            System.out.println(1);
-        }
-        {
-            System.out.println(2);
-        }
-        {
-            System.out.println(3);
-        }
-    }
 }
 
